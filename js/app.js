c8eda00d92d991773ce82ce07a2a336667340657
John Smicklo <204639360+nitrocodebrew@users.noreply.github.com>
2026-08-17 18:07:44 -0400

    Create querySelector wrapper + attach UI selectors

---
+++ js/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+const UI = {
+    dateContainer: $('.date'),
+    weekDay: $('.week-day'),
+    monthDay: $('.month-day'),
+    year: $('.year'),
+    timeContainer: $('.time'),
+    currentTime: $('.current-time'),
+    amPm: $('.am-pm'),
+};
